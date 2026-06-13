4d15d2bbaec80d3f2d03a7314372e57a86eba494
avi-071-coder <abhishek.prime071@gmail.com>
2026-06-13 12:48:00 +0530

    Update project

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,3 +1,3 @@
 const serverless = require('serverless-http');
 const app = require('../../server'); // Imports the Express app
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
